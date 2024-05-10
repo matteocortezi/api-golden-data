60040ae3688ae3347e42fbd3046b898ad848d989
Mateus Silva <developer.mateusv@gmail.com>
2024-05-10 20:04:21 -0300

    Commit de teste

---
+++ src/test/java/golden/data/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package golden.data.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
