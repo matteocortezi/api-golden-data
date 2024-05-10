80e336571711b23a4270aad29ce1784adc6f863b
Mateus Silva <developer.mateusv@gmail.com>
2024-05-10 20:42:54 -0300

    Verificando se está tudo correto com as Branchs.

--- src/main/java/golden/data/api/ApiApplication.java
+++ src/main/java/golden/data/api/ApiApplication.java
@@ -10,6 +10,6 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 	public String olaMundo (){
-		return "O mundo do git é difícil!";
+		return "O mundo do git é bem difícil!";
 	}
 }
