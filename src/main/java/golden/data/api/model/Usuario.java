0102a75cfac2bf0c1c05b9ee02e811af0aa49836
matteocortezi <developer.mateusv@gmail.com>
2024-09-14 12:13:20 -0300

    A entidade "Usuario" foi criada e corretamente configurada para o funcionamento do projeto.

--- src/main/java/golden/data/api/model/Usuario.java
+++ src/main/java/golden/data/api/model/Usuario.java
@@ -5,15 +5,56 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "usuario")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     private Long id;
     private String login;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USUARIO"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
