fef80b80376968804ed255945284eac09b53cf24
matteocortezi <developer.mateusv@gmail.com>
2024-09-14 13:03:15 -0300

    O controle de acesso da aplicação foi criado, bem como os filtros de segurança, recuperação do token e a validação do Token que foi recebido.

--- src/main/java/golden/data/api/security/TokenService.java
+++ src/main/java/golden/data/api/security/TokenService.java
@@ -3,6 +3,7 @@ package golden.data.api.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import golden.data.api.model.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,20 @@ public class TokenService {
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+    @Value("${JWT_SECRET:12345678}")
+    private String senhaToken;
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(senhaToken);
+            return JWT.require(algorithm)
+                    .withIssuer("FIAP").
+                    build().
+                    verify(token).
+                    getSubject();
+        } catch (JWTVerificationException e){
+            throw new RuntimeException("Não foi possível validar o TokenJWT");
+        }
     }
 }
