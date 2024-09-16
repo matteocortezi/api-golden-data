fba46889cf36085a31cf5e97d452d1ac8ac41021
matteocortezi <developer.mateusv@gmail.com>
2024-09-16 17:42:19 -0300

    O controller de login foi corretamente documentado

--- src/main/java/golden/data/api/controller/AutenticacaoController.java
+++ src/main/java/golden/data/api/controller/AutenticacaoController.java
@@ -4,6 +4,13 @@ import golden.data.api.dto.autenticacaoDTO.AutenticacaoDTO;
 import golden.data.api.model.Usuario;
 import golden.data.api.security.DadosTokenJWT;
 import golden.data.api.security.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,21 +23,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
-public class AutenticacaoController
-{
+@Tag(name = "Autenticação", description = "Operações relacionadas à autenticação de usuários")
+public class AutenticacaoController {
+
     @Autowired
     private AuthenticationManager manager;
 
     @Autowired
     private TokenService tokenService;
 
-
+    @Operation(summary = "Efetuar login", description = "Autentica o usuário e retorna um token JWT para uso nas requisições futuras")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login realizado com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = DadosTokenJWT.class))),
+            @ApiResponse(responseCode = "400", description = "Credenciais inválidas"),
+            @ApiResponse(responseCode = "401", description = "Não autorizado")
+    })
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid AutenticacaoDTO dados){
+    public ResponseEntity<DadosTokenJWT> efetuarLogin(
+            @RequestBody @Valid @Parameter(description = "Dados de login do usuário", required = true) AutenticacaoDTO dados) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(authenticationToken);
         var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
         return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
-
     }
 }
